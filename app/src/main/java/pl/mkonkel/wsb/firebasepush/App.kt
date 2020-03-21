17e3fc3f22788c8e2d61b4560d5c028fc26f617c
Donord <d.rogala.95@gmail.com>
2020-03-21 10:47:07 +0100

    Implementation of firebase SDK, first configuration. Sending first test push.

--- app/src/main/java/pl/mkonkel/wsb/firebasepush/App.kt
+++ app/src/main/java/pl/mkonkel/wsb/firebasepush/App.kt
@@ -1,6 +1,8 @@
 package pl.mkonkel.wsb.firebasepush
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 
 class App : Application() {
     override fun onCreate() {
@@ -8,5 +10,14 @@ class App : Application() {
 
 // TODO: Add FirebaseInstanceId Callback here
 //        It is goot to log or print the token somewhere - it will be needed later.
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    Log.e("App", "Nie udało się uzyskać FCM Token")
+                }
+
+                val token = it.result?.token
+                Log.d("App", "FCM Token: $token")
+            }
     }
 }
